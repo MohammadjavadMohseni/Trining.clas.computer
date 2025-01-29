e4aeb65882c3ebc87bfebafea30ee4e265cb8315
MohammadjavadMohseni <mohsenealiabad@gmail.com>
2025-01-29 19:18:33 +0330

    Add files via upload

--- MirrorWrite.java
+++ MirrorWrite.java
@@ -4,21 +4,29 @@ public class MirrorWrite {
     
     public String mirror(String x) {
         int a = x.length();
-        String z = ".";
-            while (a >= 0) {
-                char b = x.charAt(--a);
-                String c = x.formatted(this, b);
-                int y = c.lastIndexOf(c);
-                char d = c.charAt(y + a);
-                String s = c.formatted(this, d);
-                return z.concat(s);
+        String z = "";
+        if (x == null) {
+            z = null;
+            
+        } else if (x == "") {
+            z = "";
+            
+        } else {
+            while (a > 0) {
+                z = z + x.charAt(a = a - 1);
             }
-        String mirror = "..";
-        return mirror.concat(z);
+        }
+        return z;
     }
     
     public static void main(String[] args) {
         MirrorWrite m = new MirrorWrite();
+        MirrorWrite o = new MirrorWrite();
+        MirrorWrite i = new MirrorWrite();
+        MirrorWrite r = new MirrorWrite();
         System.err.println(" ==> " + m.mirror("xsdfefrrbfv"));
+        System.err.println(" ==> " + o.mirror("a"));
+        System.err.println(" ==> " + i.mirror(""));
+        System.err.println(" ==> " + r.mirror(null));
     }
 }
