f77ac6200bdea5dacd10c772d1d7ad326587cc29
MohammadjavadMohseni <mohsenealiabad@gmail.com>
2025-01-30 18:23:37 +0330

    Add files via upload

--- MirrorWrite.java
+++ MirrorWrite.java
@@ -1,5 +1,7 @@
 package trainig.string;
 
+import java.lang.reflect.Array;
+
 public class MirrorWrite {
     
     public String mirror(String x) {
@@ -20,11 +22,51 @@ public class MirrorWrite {
         return z;
     }
     
+    public Array mirrorArray(String x) {
+        Array arr[];
+        if (x == null) {
+            arr = null;
+                    
+        } else if (x.isEmpty()) {
+            arr = [""];
+            
+        } else {
+            int a = x.length();
+            while (a > 0) {
+                a = a - 1;
+                arr = Array.get(x, a);
+                Array.set(arr, a, a);
+            }
+        }
+        return arr;
+    }
+    
+    public StringBuilder mirrorStringBuilder(String x) {
+        String y = "";
+        StringBuilder z;
+        if (x == null) {
+            z = null;
+        } else if (x.isBlank()) {
+            z.append("");
+        } else {
+            int a = x.length();
+            while (a > 0) {
+                a = a - 1;
+                z = z.append(a);
+            }
+        }
+        return z;
+    }
+    
     public static void main(String[] args) {
         MirrorWrite m = new MirrorWrite();
         System.err.println(" ==> " + m.mirror("xsdfefrrbfv"));
         System.err.println(" ==> " + m.mirror("a"));
         System.err.println(" ==> " + m.mirror(""));
         System.err.println(" ==> " + m.mirror(null));
+        System.err.println(" ==> " + m.mirrorArray("xsdfefrrbfv"));
+        System.err.println(" ==> " + m.mirrorArray("a"));
+        System.err.println(" ==> " + m.mirrorArray(""));
+        System.err.println(" ==> " + m.mirrorArray(null));
     }
 }
