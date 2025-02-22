03321735e0eb53fb88c1054f6ec0ee8c2629580a
MohammadjavadMohseni <mohsenealiabad@gmail.com>
2025-02-22 20:25:19 +0330

    Add files via upload

--- MirrorWrite.java
+++ MirrorWrite.java
@@ -24,16 +24,18 @@ public class MirrorWrite {
             
         } else {
             int a = x.length();
-            while (a > 0) {
-                a = a - 1;
-                
+            char[] l = new char[a];
+            String y = new String(l);
+            for (int b = l.length; b > 0; b--) {
+                char[] m = y.toCharArray();
+                z = new String(m);
             }
         }
         return z;
     }
     
     public String mirrorStringBuilder(String x) {
-        String y = "";
+        String y;
         StringBuilder z = new StringBuilder();
         if (x == null || x.isBlank()) {
             y = x;
@@ -44,7 +46,7 @@ public class MirrorWrite {
                 a = a - 1;
                 z.append(x.charAt(a));
             }
-            y = y + z;
+            y = z.toString();
         }
         return y;
     }
