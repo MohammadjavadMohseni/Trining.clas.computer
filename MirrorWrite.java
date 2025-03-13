f158d8dc4793533c4bef3e4e8941bddb139ff439
MohammadjavadMohseni <mohsenealiabad@gmail.com>
2025-03-13 16:50:23 +0330

    Add files via upload

--- MirrorWrite.java
+++ MirrorWrite.java
@@ -1,7 +1,7 @@
-package trainig.string;
+package training.string;
 
 public class MirrorWrite {
-    
+
     public String mirror(String x) {
         String z = "";
         if (x == null || x.isBlank()) {
@@ -82,168 +82,149 @@ public class MirrorWrite {
         return t;
     }
     
-    public StringBuilder nameNumber(int n) {
+    public StringBuilder nameNumber(long n) {
         StringBuilder z = new StringBuilder();
         StringBuilder y = new StringBuilder();
         z.append(n);
         int a = z.length();
         
+        if (n < 0) {
+            y.append("منفی ");
+            n = -1 * n;
+            
+        }
+        if (n == 0) {
+            y.append("صفر ");
+        }
+            
         while (a > 0) {
             double b = Math.pow(10, a - 1);
-            int c = (int) (n / b);
-            
-            if (n == 0) {
-                y.append("صفر");
-            
-        } else if (n > 0) {
+            int c =  (int) (n / b);
             
             if (a % 3 == 0) {
                 int d = c % 10;
                 switch (d) {
                     
-                    case 0:y.append("");
-                    break;
-                    case 1:y.append("صد ");
-                    break;
-                    case 2:y.append("دویست ");
-                    break;
-                    case 3:y.append("سیصد ");
-                    break;
-                    case 4:y.append("چهارصد ");
-                    break;
-                    case 5:y.append("پانصد ");
-                    break;
-                    case 6:y.append("شیشصد ");
-                    break;
-                    case 7:y.append("هفتصد ");
-                    break;
-                    case 8:y.append("هشتصد ");
-                    break;
-                    case 9:y.append("نهصد ");
-                    break;
+                    case 0 -> y.append("");
+                    case 1 -> y.append("صد ");
+                    case 2 -> y.append("دویست ");
+                    case 3 -> y.append("سیصد ");
+                    case 4 -> y.append("چهارصد ");
+                    case 5 -> y.append("پانصد ");
+                    case 6 -> y.append("شیشصد ");
+                    case 7 -> y.append("هفتصد ");
+                    case 8 -> y.append("هشتصد ");
+                    case 9 -> y.append("نهصد ");
                 }
                 
             } else if ((a - 2) == 3 * ((a - 2) / 3)) {
                 int d = c % 10;
                 switch (d) {
                     
-                    case 0:y.append("");
-                    break;
-                    case 1:y.append("ده ");
-                    break;
-                    case 2:y.append("بیست ");
-                    break;
-                    case 3:y.append("سی ");
-                    break;
-                    case 4:y.append("چهل ");
-                    break;
-                    case 5:y.append("پنجاه ");
-                    break;
-                    case 6:y.append("شصت ");
-                    break;
-                    case 7:y.append("هفتاد ");
-                    break;
-                    case 8:y.append("هشتاد ");
-                    break;
-                    case 9:y.append("نود ");
-                    break;
+                    case 0 -> y.append("");
+                    case 1 -> {
+                        b = Math.pow(10, a - 1);
+                        c = (int) (n / b);
+                        d = c % 100;
+                        switch (d) {
+                            case 10 -> y.append("ده ");
+                            case 11 -> y.append("یازده ");
+                            case 12 -> y.append("دوازده ");
+                            case 13 -> y.append("سیزده ");
+                            case 14 -> y.append("چهارده ");
+                            case 15 -> y.append("پانزده ");
+                            case 16 -> y.append("شانزده ");
+                            case 17 -> y.append("هفده ");
+                            case 18 -> y.append("هجده ");
+                            case 19 -> y.append("نوزده ");
+                        }
+                    }
+                    case 2 -> y.append("بیست ");
+                    case 3 -> y.append("سی ");
+                    case 4 -> y.append("چهل ");
+                    case 5 -> y.append("پنجاه ");
+                    case 6 -> y.append("شصت ");
+                    case 7 -> y.append("هفتاد ");
+                    case 8 -> y.append("هشتاد ");
+                    case 9 -> y.append("نود ");
                 }
                 
             } else {
                 int d = c % 10;
                 switch (d) {
                     
-                    case 0:y.append("");
-                    break;
-                    case 1:y.append("یک ");
-                    break;
-                    case 2:y.append("دو ");
-                    break;
-                    case 3:y.append("سه ");
-                    break;
-                    case 4:y.append("چهار ");
-                    break;
-                    case 5:y.append("پنج ");
-                    break;
-                    case 6:y.append("شیش ");
-                    break;
-                    case 7:y.append("هفت ");
-                    break;
-                    case 8:y.append("هشت ");
-                    break;
-                    case 9:y.append("نه ");
-                    break;
+                    case 0 -> y.append("");
+                    case 1 -> y.append("یک ");
+                    case 2 -> y.append("دو ");
+                    case 3 -> y.append("سه ");
+                    case 4 -> y.append("چهار ");
+                    case 5 -> y.append("پنج ");
+                    case 6 -> y.append("شیش ");
+                    case 7 -> y.append("هفت ");
+                    case 8 -> y.append("هشت ");
+                    case 9 -> y.append("نه ");
                 }
                 
                 if (d >= 0) {
                     
                     switch (a) {
                     
-                    case 4:y.append("هزار ");
-                    break;
-                    case 7:y.append("میلیون ");
-                    break;
-                    case 10:y.append("میلیارد ");
-                    break;
-                    default:y.append("");
+                    case 4 -> y.append("هزار ");
+                    case 7 -> y.append("میلیون ");
+                    case 10 -> y.append("میلیارد ");
+                    default -> y.append("");
                 }
 
-                }
             }
             
-        } else {
-            
-            y.append("منفی ");
-            n = -1 * n;
         }
-            
             a--;
-        }
-            
+        } 
         return y;
+        }
     }
     
     public static void main(String[] args) {
-        MirrorWrite m = new MirrorWrite();
+        MirrorWrite s = new MirrorWrite();
         long l1 = System.currentTimeMillis();
-        m.testMirror("xsdfefrrbfv", 100);
+        s.testMirror("xsdfefrrbfv", 100);
         System.err.println(" a : " + (System.currentTimeMillis() - l1) + "ms");
         System.err.println("_______________");
-        m.mirror(m.number(10));
+        s.mirror(s.number(10));
         System.err.println(" a : " + (System.currentTimeMillis() - l1) + "ms");
         System.err.println("_______________");
-        System.err.println(" ==> " + m.mirror("xsdfefrrbfv"));
-        System.err.println(" ==> " + m.mirror("a"));
-        System.err.println(" ==> " + m.mirror(""));
-        System.err.println(" ==> " + m.mirror(null));
+        System.err.println(" ==> " + s.mirror("xsdfefrrbfv"));
+        System.err.println(" ==> " + s.mirror("a"));
+        System.err.println(" ==> " + s.mirror(""));
+        System.err.println(" ==> " + s.mirror(null));
         long l2 = System.currentTimeMillis();
-        m.testMirrorStringBuilder("xsdfefrrbfv", 100);
+        s.testMirrorStringBuilder("xsdfefrrbfv", 100);
         System.err.println(" a : " + (System.currentTimeMillis() - l2) + "ms");
         System.err.println("_______________");
-        m.mirrorStringBuilder(m.number(10));
+        s.mirrorStringBuilder(s.number(10));
         System.err.println(" a : " + (System.currentTimeMillis() - l2) + "ms");
         System.err.println("________________");
-        System.err.println(" ==> " + m.mirrorStringBuilder("xsdfefrrbfv"));
-        System.err.println(" ==> " + m.mirrorStringBuilder("a"));
-        System.err.println(" ==> " + m.mirrorStringBuilder(""));
-        System.err.println(" ==> " + m.mirrorStringBuilder(null));
+        System.err.println(" ==> " + s.mirrorStringBuilder("xsdfefrrbfv"));
+        System.err.println(" ==> " + s.mirrorStringBuilder("a"));
+        System.err.println(" ==> " + s.mirrorStringBuilder(""));
+        System.err.println(" ==> " + s.mirrorStringBuilder(null));
         long l3 = System.currentTimeMillis();
-        m.testMirrorArray("xsdfefrrbfv", 100);
+        s.testMirrorArray("xsdfefrrbfv", 100);
         System.err.println(" a : " + (System.currentTimeMillis() - l3) + "ms");
         System.err.println("____________");
-        m.mirrorArray(m.number(10));
+        s.mirrorArray(s.number(10));
         System.err.println(" a : " + (System.currentTimeMillis() - l3) + "ms");
         System.err.println("______________");
-        System.err.println(" ==> " + m.mirrorArray("xsdfefrrbfv"));
-        System.err.println(" ==> " + m.mirrorArray("a"));
-        System.err.println(" ==> " + m.mirrorArray(""));
-        System.err.println(" ==> " + m.mirrorArray(null));
+        System.err.println(" ==> " + s.mirrorArray("xsdfefrrbfv"));
+        System.err.println(" ==> " + s.mirrorArray("a"));
+        System.err.println(" ==> " + s.mirrorArray(""));
+        System.err.println(" ==> " + s.mirrorArray(null));
         System.err.println("_____________________________");
-        System.err.println(" ==> " + m.nameNumber(123456789));
-        System.err.println(" ==> " + m.nameNumber(100000000));
-        System.err.println(" ==> " + m.nameNumber(100000));
-        System.err.println(" ==> " + m.nameNumber(1000000000));
-        System.err.println(" ==> " + m.nameNumber(0));
-        System.err.println(" ==> " + m.nameNumber(-123456789));
+        System.err.println(" ==> " + s.nameNumber(123456789L));
+        System.err.println(" ==> " + s.nameNumber(100000000L));
+        System.err.println(" ==> " + s.nameNumber(100000L));
+        System.err.println(" ==> " + s.nameNumber(100000000000L));
+        System.err.println(" ==> " + s.nameNumber(0L));
+        System.err.println(" ==> " + s.nameNumber(-123456789L));
+
     }
-}
